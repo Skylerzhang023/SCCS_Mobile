f06f14ef0f6e27f026191f0a1821e6e011042a52
Yitian Zhang <skyler2zhang@gmail.com>
2017-03-25 18:41:23 -0700

    auto_save

--- app/src/main/java/com/example/skycro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/skycro/myapplication/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.skycro.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View.OnClickListener;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.app.Activity;
+
 public class MainActivity extends AppCompatActivity {
+    private Button btnshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnshow = (Button) findViewById(R.id.button);
+        btnshow.setOnClickListener(new OnClickListener() {
+            //重写点击事件的处理方法onClick()
+            @Override
+            public void onClick(View v) {
+                //显示Toast信息
+                Toast.makeText(getApplicationContext(), "你点击了按钮", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
