e601f520b59eb54cf7fb3cd3060e36f012ccf9f5
Yitian Zhang <skyler2zhang@gmail.com>
2017-03-31 11:09:12 -0700

    login

--- app/src/main/java/com/example/skycro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/skycro/myapplication/MainActivity.java
@@ -8,26 +8,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnshow;
+    private Button login;
+    private EditText account,userpassword;
+    private CheckBox checkBox;
+    String user,password;
+    private String addr = "http://192.168.2.101:80/serlet/loginServlet";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnshow = (Button) findViewById(R.id.button);
-        btnshow.setOnClickListener(new OnClickListener() {
-            //重写点击事件的处理方法onClick()
+        login = (Button) findViewById(R.id.button);
+        account = (EditText) findViewById(R.id.account);
+        userpassword = (EditText) findViewById(R.id.userpassword);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        //重写点击事件的处理方法onClick()
+        login.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                //显示Toast信息
-                Toast.makeText(getApplicationContext(), "你点击了按钮", Toast.LENGTH_SHORT).show();
+                user = account.getText().toString();
+                password = userpassword.getText().toString();
+                //check if user and password is blank
+                if(("".equals(user)))
+				{
+					Toast.makeText(getApplicationContext(),"用户名不能为空", Toast.LENGTH_LONG).show();
+				}
+				if(("".equals(password)))
+				{
+					Toast.makeText(getApplicationContext(),"密码不能为空", Toast.LENGTH_LONG).show();
+				}
             }
         });
+
     }
 
 
