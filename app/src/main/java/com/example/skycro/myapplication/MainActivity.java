cf49528c93ff9b0e510626d1b2239e1647feb345
Yitian Zhang <skyler2zhang@gmail.com>
2017-04-06 02:21:25 -0700

    wow

--- app/src/main/java/com/example/skycro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/skycro/myapplication/MainActivity.java
@@ -15,6 +15,10 @@ import android.widget.Toast;
 import android.app.Activity;
 import android.widget.RadioButton;
 
+import android.content.Intent;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -26,21 +30,23 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     private EditText account,userpassword;
     private CheckBox checkBox;
+    private HttpURLConnection Connection;
     String user,password,param;
     private URL posturl;
     private String address = "http://121.40.34.92:7070/web/api/json?cmd=login&ctrl=user&version=1&1lang=zh_CN";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //get the ites registered
         login = (Button) findViewById(R.id.button);
         account = (EditText) findViewById(R.id.account);
         userpassword = (EditText) findViewById(R.id.userpassword);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
+
 
         //重写点击事件的处理方法onClick()
         login.setOnClickListener(new OnClickListener() {
@@ -58,18 +64,6 @@ public class MainActivity extends AppCompatActivity {
 					Toast.makeText(getApplicationContext(),"密码不能为空", Toast.LENGTH_LONG).show();
 				}
 				else{
-                    String params = "user"+":"+ user + "password" + "+" + password;
-                    //set the url
-                    try {
-                        posturl = new URL(address);
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        URLConnection rulConnection = posturl.openConnection();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
 
 
 
---
+++ app/src/main/java/com/example/skycro/myapplication/MapsActivity.java
@@ -0,0 +1,93 @@
+package com.example.skycro.myapplication;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import 	android.content.Context;
+import  android.location.*;
+import  android.util.*;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    int MY_PERMISSIONS_REQUEST_LOCATION;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mMap = googleMap;
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.READ_CONTACTS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double latitude = location.getLatitude();     //经度
+        double longitude = location.getLongitude(); //纬度
+        double altitude =  location.getAltitude();     //海拔
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("I'm HERE!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+
+
+}
